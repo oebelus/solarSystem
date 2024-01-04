0abc7ed14d1e27e759fbe61ec79b7f8607cd6ac1
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-01-04 18:44:02 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,10 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(width, height)
 renderer.shadowMap.enabled = true;
 
-const camera = new THREE.PerspectiveCamera(45, width/height, 0.5, 1000)
-camera.position.z = 120
+const camera = new THREE.PerspectiveCamera(30, width/height, 0.5, 1000)
+camera.position.z = 150
+camera.position.y = -100
+camera.position.x = -150
 
 const scene = new THREE.Scene()
 
@@ -29,6 +31,8 @@ controls.minDistance = 50;
 controls.maxDistance = 500;
 
 controls.maxPolarAngle = Math.PI / 2;
+
+
 
 class Planet {
   geometry: any
@@ -98,7 +102,7 @@ class Ring {
       this.material = new THREE.MeshStandardMaterial({ map: texture });
     }
     
-    else this.material = new THREE.MeshBasicMaterial({ color: "white" });
+    else this.material = new THREE.MeshBasicMaterial({ color: "#818589" });
     
     this.element = new THREE.Mesh(this.geometry, this.material)
   }
@@ -115,15 +119,15 @@ class Ring {
 
 let data: Record<string, (string|number)[]> = {
   "sun": ["https://i.imgur.com/NT8Vd4p.jpg", 274, 696340],
-  "mercury": ["https://i.imgur.com/UQQRIZg.jpg", 3.7, 2439.7, 0.39 * 27, 47.9],
+  "mercury": ["https://i.imgur.com/UQQRIZg.jpg", 3.7, 2439.7, 0.39 * 32, 47.9],
   "venus": ["https://i.imgur.com/Wa8lpF6.jpg", 8.87, 6051.8, 0.72 * 26, 35],
   "earth": ["https://i.imgur.com/Tajyxwl.jpg", 9.807, 6371, 1 * 28, 29.8],
   "moon": ["https://i.imgur.com/OjvY5Pv.jpg", 1.62],
-  "mars": ["https://i.imgur.com/U2QZveA.jpg", 3.71, 3389.5, 1.524, 24],
-  "jupiter": ["https://i.imgur.com/ZUVwSv5.jpg", 24.79, 69911, 5.2, 13.1],
-  "saturn": ["https://i.imgur.com/ITtPqGy.jpg", 10.44, 58232, 9.54, 9.69],
-  "uranus": ["https://i.imgur.com/wj611Q1.jpg", 8.87, 25362, 19.22, 6.81],
-  "neptune": ["https://i.imgur.com/KU2X0rf.jpg", 11.15, 24622, 30.06, 5.43]
+  "mars": ["https://i.imgur.com/U2QZveA.jpg", 3.71, 3389.5, 1.524 * 25, 24],
+  "jupiter": ["https://i.imgur.com/ZUVwSv5.jpg", 24.79, 69911, 5.2 * 9.5, 13.1],
+  "saturn": ["https://i.imgur.com/ITtPqGy.jpg", 10.44, 58232, 9.54 * 6.5, 9.69],
+  "uranus": ["https://i.imgur.com/wj611Q1.jpg", 8.87, 25362, 19.22 * 4, 6.81],
+  "neptune": ["https://i.imgur.com/KU2X0rf.jpg", 11.15, 24622, 30.06 * 3, 5.43]
 }
 
 // Create an elliptical curve representing the orbit
@@ -138,7 +142,7 @@ function orbit(xRadius: number, yRadius: number): THREE.Line<THREE.BufferGeometr
   );
   
   // Create a geometry from the path
-  let points = orbitPath.getPoints(50);
+  let points = orbitPath.getPoints(200);
   let geometry = new THREE.BufferGeometry().setFromPoints(points);
   
   let material = new THREE.LineBasicMaterial({ color : "grey" });
@@ -152,6 +156,11 @@ function drawPlanet(planet: Planet, orbit: THREE.Line<THREE.BufferGeometry<THREE
   planet.draw()
   orbit.add(planet.element)
   scene.add(orbit)
+}
+
+function rotate(planet: Planet) {
+  planet.element.rotation.x += 0.01
+  planet.element.rotation.y += 0.01
 }
 
 let sun = new Planet(data["sun"][2] as number / 80000, data["sun"][0] as string, 0, 0, 0, true);
@@ -172,28 +181,28 @@ let earthOrbit = orbit(data["earth"][3] as number, data["earth"][3] as number)
 drawPlanet(earth, earthOrbit)
 /*let moon = new Planet(earthRadius - 5, data["moon"][0] as string)*/
 
-let mars = new Planet(data["mars"][2] as number / 2000, data["mars"][0] as string, data["mars"][3] as number * 25, 0, 0)
-let marsOrbit = orbit(data["mars"][3] as number * 25, data["mars"][3] as number * 25)
+let mars = new Planet(data["mars"][2] as number / 2000, data["mars"][0] as string, data["mars"][3] as number, 0, 0)
+let marsOrbit = orbit(data["mars"][3] as number, data["mars"][3] as number)
 drawPlanet(mars, marsOrbit)
 
 // Jupiter
-let jupiter = new Planet(data["jupiter"][2] as number / 11000, data["jupiter"][0] as string, data["jupiter"][3] as number * 10, 2, 0)
-let jupiterOrbit = orbit(data["jupiter"][3] as number * 10, data["jupiter"][3] as number * 10)
+let jupiter = new Planet(data["jupiter"][2] as number / 11000, data["jupiter"][0] as string, data["jupiter"][3] as number, 2, 0)
+let jupiterOrbit = orbit(data["jupiter"][3] as number, data["jupiter"][3] as number)
 drawPlanet(jupiter, jupiterOrbit)
 
 // Saturn
-let saturn = new Planet(data["saturn"][2] as number / 11000, data["saturn"][0] as string, data["saturn"][3] as number * 8, 2, 0)
-let saturnOrbit = orbit(data["saturn"][3] as number * 8, data["saturn"][3] as number * 8)
+let saturn = new Planet(data["saturn"][2] as number / 11000, data["saturn"][0] as string, data["saturn"][3] as number, 2, 0)
+let saturnOrbit = orbit(data["saturn"][3] as number, data["saturn"][3] as number)
 drawPlanet(saturn, saturnOrbit)
 
 // Uranus
-let uranus = new Planet(data["uranus"][2] as number / 11000, data["uranus"][0] as string, data["uranus"][3] as number * 5, 3, 0)
-let uranusOrbit = orbit(data["uranus"][3] as number * 5, data["uranus"][3] as number * 5)
+let uranus = new Planet(data["uranus"][2] as number / 11000, data["uranus"][0] as string, data["uranus"][3] as number, 3, 0)
+let uranusOrbit = orbit(data["uranus"][3] as number, data["uranus"][3] as number)
 drawPlanet(uranus, uranusOrbit)
 
 // Neptune
-let neptune = new Planet(data["neptune"][2] as number / 11000, data["neptune"][0] as string, data["neptune"][3] as number*4, 3, 0)
-let neptuneOrbit = orbit(data["neptune"][3] as number*4, data["neptune"][3] as number*4)
+let neptune = new Planet(data["neptune"][2] as number / 11000, data["neptune"][0] as string, data["neptune"][3] as number, 3, 0)
+let neptuneOrbit = orbit(data["neptune"][3] as number, data["neptune"][3] as number)
 drawPlanet(neptune, neptuneOrbit)
 
 // LIGHTS
@@ -222,13 +231,28 @@ function animate() {
   sun.draw()
 
   mercuryOrbit.rotation.z -= data["mercury"][4] as number / 4000
+  rotate(mercury)
+
   venusOrbit.rotation.z -= data["venus"][4] as number / 4000
+  rotate(venus)
+
   earthOrbit.rotation.z -= data["earth"][4] as number / 4000
+  rotate(earth)
+
   marsOrbit.rotation.z -= data["mars"][4] as number / 4000
+  rotate(mars)
+
   jupiterOrbit.rotation.z -= data["jupiter"][4] as number / 4000
+  rotate(jupiter)
+
   saturnOrbit.rotation.z -= data["saturn"][4] as number / 4000
+  rotate(saturn)
+
   uranusOrbit.rotation.z -= data["uranus"][4] as number / 4000
+  rotate(uranus)
+
   neptuneOrbit.rotation.z -= data["neptune"][4] as number / 4000
+  rotate(neptune)
 
   renderer.render(scene, camera)
 }
