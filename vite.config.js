83f2a2299ac069fe75aa01560a9998b99c88c432
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-01-06 22:11:01 +0100

    ll

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default {
+  plugins: [vanillaExtractPlugin()],
+  base: "https://github.com/xertendsz/solarSystem",
+};
