71233722e4d97233b2bca03984941379d8510d4f
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-01-06 22:18:36 +0100

    adding dist

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-
-export default {
-  plugins: [vanillaExtractPlugin()],
-  base: "https://github.com/xertendsz/solarSystem",
-};
